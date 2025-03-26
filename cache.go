4b251dfe21737866fc7d9df73899ff7f7e8ac786
G00dBunny <bradleysanchez28@gmail.com>
2025-03-26 19:37:33 -0400

    Added : Set item

--- cache.go
+++ cache.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
@@ -17,7 +18,7 @@ type Cache struct {
 }
 
 type cache struct {
-	namespace 		map[string]Pod
+	namespaces 		map[string]Pod
 	expirationTime 	time.Duration 
 	lock 			sync.Mutex
 }
@@ -25,17 +26,42 @@ type cache struct {
 func New(expiration time.Duration) *Cache{
 	return &Cache{
 		cache: &cache{
-			namespace: 		make(map[string]Pod),
+			namespaces: 		make(map[string]Pod),
 			expirationTime: expiration,
 		},
 	}
 }	
 
-func (c *cache) Set (namespace string, pod string) {}
+
+// TODO : add a force parameter for updating? 
+func (c *cache) Set (namespace string, pod []string) error {
+	c.lock.Lock()
+
+	if _,exists := c.namespaces[namespace]; exists {
+		c.lock.Unlock()
+		return fmt.Errorf("item %s already exists", namespace)
+	}
+
+	c.namespaces[namespace] = Pod{
+		pods: pod,
+	}
+	c.lock.Unlock()
+
+	return nil
+}
 
 func (c *cache) Add (namespace string, pod string) {}
 
 func (c *cache) Delete (namespace string, pod string){}
+
+//TODO : Add a flushing option
+
+//TODO : Add a janitor smurf using a tick that will check if items are expired
+
+//TODO : Add a expiration smurf that will expire all namespaces
+
+//TODO : Add a replacer smurf that will replace data
+
 
 func Refresh() *Cache{
 	return &Cache{}
